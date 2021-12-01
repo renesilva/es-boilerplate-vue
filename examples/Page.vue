c523d0d00c238d3cfe846c06a2003e4700d09f4f
Eressea RS <rsilva@eresseasolutions.com>
2021-12-01 16:10:26 -0400

    Componentes y Páginas de ejemplo

---
+++ examples/Page.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'ExamplePage',
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  // Se pueden utilizar estos hooks para el ciclo de vida
+  // beforeCreate, created, beforeMount, mounted, beforeUpdate, updated
+  // activated, deactivated, beforeUnmount, unmounted
+};
+</script>
+
+<style scoped></style>
